2fb2e03dcd77a8974b72eddf943962dbe1f84f34
michigun.xyz <fp3discord@gmail.com>
2026-01-31 17:28:12 -0300

    Update path.lua

--- path.lua
+++ path.lua
@@ -2,7 +2,7 @@ return {
     silent = "silent.lua",
     hitbox = "hitbox.lua",
     esp = "esp.lua",
-    tas = "tas2.lua",
+    tas = "tas.lua",
     jj = "jj.lua",
     f3x = "f3x.lua",
     char = "char.lua",
